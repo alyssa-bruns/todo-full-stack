e5dcd882fb905b8f990b993f24e9f64eada56f0d
Alyssa <alynicbru@gmail.com>
2024-03-29 21:36:12 +1300

    Refactor

--- client/components/TodoList.tsx
+++ client/components/TodoList.tsx
@@ -1,14 +1,8 @@
 import useFetchTodos from '../hooks/use-fetch-todos'
-import useDeleteTodos from "../hooks/use-delete-todos"
-import useEditTodo from '../hooks/use-edit-todo'
-import { useState} from 'react'
-import { Todos } from '../models/todos'
 import TodoListItem from './TodoListItem'
 
 function TodoList() {
   const {data: todos, isPending, isError, error } = useFetchTodos()
-  
-  const deleteTodo= useDeleteTodos()
 
   if (isPending) {
     return <p>Loading...</p>
@@ -23,10 +17,7 @@ function TodoList() {
     )
   }
 
-  const handleDelete = (id: number) => {
-    deleteTodo.mutate(id)
-  }
-
+ 
   return (
     <>
     <ul className="todo-list"  >
