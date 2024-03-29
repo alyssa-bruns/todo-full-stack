0ca12b75d7d2310b538a237cd1261b9fc136d660
Alyssa <alynicbru@gmail.com>
2024-03-29 22:40:02 +1300

    Start logic in CompletedTask component to mark a task as complete

--- client/components/CompleteTask.tsx
+++ client/components/CompleteTask.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
+import { completeTodo } from "../apis/apiClient"
+import { Todos } from "../models/todos"
+import useCompleteTodo from "../hooks/use-complete-todos"
 
 
-function CompleteTask() {
+function CompleteTask(props: Todos) {
+    const completeTask = useCompleteTodo()
+    const [isCompleted, setIsCompleted] = useState(false)
 
 
+       const handleCompletedClick = (e:React.MouseEvent) => {
+        e.preventDefault()
+        setIsCompleted(true)
+        completeTask.mutate(props.id)
+      }
     return(
         <>
-        <input className="toggle" type="checkbox"/>
+        <input onClick={()=>handleCompletedClick(props.id)} className="toggle" type="checkbox"/>
         </>
     )
 }
