6a6459b42c1a965bed25c4f89d83bc1e621655e7
Alyssa <alynicbru@gmail.com>
2024-04-01 12:00:35 +1300

    Create db test to createTodo

--- server/db/db.test.ts
+++ server/db/db.test.ts
@@ -1,3 +1,30 @@
 import * as db from './db.js'
 import { describe, it, expect, beforeAll, beforeEach, afterAll } from 'vitest'
 import connection from './connection.ts'
+
+beforeAll(() => {
+  return connection.migrate.latest()
+})
+
+beforeEach(() => {
+  return connection.seed.run()
+})
+
+describe('createTodo', () => {
+  it('should add a new todo', async () => {
+    await db.createTodo({
+      id: 100,
+      name: 'Finish tests',
+      is_active: true,
+      is_completed: false,
+      priority: 1,
+    })
+    const allTodos = await db.getTodos()
+    console.log(allTodos)
+    expect(allTodos).toHaveLength(4)
+  })
+})
+
+afterAll(() => {
+  connection.destroy()
+})
